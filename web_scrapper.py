5870cdda1a0f79933df953b914ed31b1578b0ee1
Kevin <kevinm1776@gmail.com>
2020-02-04 21:20:19 -0800

    Updated parser.

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,9 +1,16 @@
+import json
+import os
+import re
 import sys
+
+from datetime import datetime
 
 import requests
 
 from bs4 import BeautifulSoup
 
+
+get_num = lambda s: re.search(r'(?<=\s)\d+', s).group()
 
 def get_html(url):
     response = requests.get(url, timeout=5)
@@ -14,14 +21,25 @@ def get_html(url):
             'author': tweet.find('h2', attrs={'class': 'author'}).text,
             'date': tweet.find('h5', attrs={'class': 'dateTime'}).text,
             'tweet': tweet.find('p', attrs={'class': 'content'}).text,
-            'likes': tweet.find('p', attrs={'class': 'likes'}).text,
-            'shares': tweet.find('p', attrs={'class': 'shares'}).text
+            'likes': get_num(tweet.find('p', attrs={'class': 'likes'}).text),
+            'shares': get_num(tweet.find('p', attrs={'class': 'shares'}).text)
         }
 
         yield tweet_data
 
+def scrape_to_json(url):
+    now = datetime.now()
+
+    current_time = now.strftime('%Y-%m-%d_%H:%M')
+    file_path = f'data/twitter_data{current_time}.json'
+
+    with open(file_path, 'a') as f:
+        for data in get_html(url):
+            json.dump(data, f)
+            f.write('\n')
+
+
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    content = get_html(url)
-    print(next(content))
+    scrape_to_json(url)
